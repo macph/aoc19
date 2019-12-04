70dafe092be1268a4eee8f9f63b2bdcedf454d47
macph <macph@users.noreply.github.com>
2019-12-04 11:14:34 +0000

    Add day 4

---
+++ src/days/d04.rs
@@ -0,0 +1,78 @@
+const MIN: u32 = 357253;
+const MAX: u32 = 892942;
+
+struct IterDigits {
+    remaining: u32,
+    finished: bool,
+}
+
+impl Iterator for IterDigits {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.finished {
+            None
+        } else {
+            let x = self.remaining % 10;
+            self.remaining /= 10;
+            if self.remaining == 0 {
+                self.finished = true;
+            }
+            Some(x as Self::Item)
+        }
+    }
+}
+
+fn iter_digits(integer: u32) -> IterDigits {
+    IterDigits {
+        remaining: integer,
+        finished: false,
+    }
+}
+
+pub fn d04a() -> String {
+    (MIN..=MAX)
+        .filter(|&pw| {
+            let mut p: Option<u8> = None;
+            let mut adj = false;
+            for d in iter_digits(pw) {
+                if p.is_some() {
+                    if p.unwrap() < d {
+                        return false;
+                    }
+                    if p.unwrap() == d {
+                        adj = true;
+                    }
+                }
+                p = Some(d);
+            }
+            adj
+        })
+        .count()
+        .to_string()
+}
+
+pub fn d04b() -> String {
+    (MIN..=MAX)
+        .filter(|&pw| {
+            let mut p: Option<u8> = None;
+            let mut pairs: Vec<u8> = Vec::new();
+            for d in iter_digits(pw) {
+                if p.is_some() {
+                    if p.unwrap() < d {
+                        return false;
+                    }
+                    if p.unwrap() == d {
+                        pairs.push(d);
+                    }
+                }
+                p = Some(d);
+            }
+            pairs
+                .iter()
+                .map(|&p| pairs.iter().filter(|&&q| p == q).count())
+                .any(|c| c == 1)
+        })
+        .count()
+        .to_string()
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,14 +1,20 @@
 mod d01;
 mod d02;
 mod d03;
+mod d04;
 
 use d01::{d01a, d01b};
 use d02::{d02a, d02b};
 use d03::{d03a, d03b};
+use d04::{d04a, d04b};
 
 pub type Solver = dyn Fn() -> String + Send + Sync + 'static;
 
 lazy_static! {
-    pub static ref SOLVERS: Vec<Vec<&'static Solver>> =
-        vec![vec![&d01a, &d01b], vec![&d02a, &d02b], vec![&d03a, &d03b],];
+    pub static ref SOLVERS: Vec<Vec<&'static Solver>> = vec![
+        vec![&d01a, &d01b],
+        vec![&d02a, &d02b],
+        vec![&d03a, &d03b],
+        vec![&d04a, &d04b],
+    ];
 }
